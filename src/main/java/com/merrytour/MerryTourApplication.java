57bd4624911d5a6fd1f7627380b1a9c4241c1c56
shinyounseob1022 <shinyounseob1022@gmail.com>
2024-01-26 20:03:41 +0900

    first commit

---
+++ src/main/java/com/merrytour/MerryTourApplication.java
@@ -0,0 +1,13 @@
+package com.merrytour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerryTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerryTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merrytour/MerryTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merrytour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerryTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
